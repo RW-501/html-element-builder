8a8be195e83a803b82607d08bf56fba04b77058f
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-06-20 14:54:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,61 +27,83 @@ document.addEventListener('DOMContentLoaded', function() {
     generatedCodeContainer.style.display = 'block';
   });
 
-  // Create a new element based on the selected type
-  function createNewElement(type) {
-    let element;
-    switch (type) {
-      case 'button':
-        element = '<button>Click Me</button>';
-        break;
-      case 'input':
-        element = '<input type="text" placeholder="Enter text">';
-        break;
-      case 'textarea':
-        element = '<textarea></textarea>';
-        break;
-      // Add more cases for other elements
-    }
-    return element;
-  }
+// ...
 
-  // Create the tools based on the selected element
-  function createTools(type) {
-    let tools;
-    switch (type) {
-      case 'button':
-        tools = 'Button Tools: <input type="text" id="buttonText" placeholder="Button Text">';
-        break;
-      case 'input':
-        tools = 'Input Tools: <input type="text" id="inputPlaceholder" placeholder="Input Placeholder">';
-        break;
-      case 'textarea':
-        tools = 'Textarea Tools: <input type="number" id="textareaRows" placeholder="Number of Rows">';
-        break;
-      // Add more cases for other elements
-    }
-    return tools;
+// Create a new element based on the selected type
+function createNewElement(type) {
+  let element;
+  switch (type) {
+    case 'button':
+      element = '<button>Click Me</button>';
+      break;
+    case 'input':
+      element = '<input type="text" placeholder="Enter text">';
+      break;
+    case 'textarea':
+      element = '<textarea></textarea>';
+      break;
+    case 'checkbox':
+      element = '<input type="checkbox"> Checkbox';
+      break;
+    case 'label':
+      element = '<label>Label</label>';
+      break;
+    // Add more cases for other elements
   }
+  return element;
+}
 
-  // Update the element based on the tool changes
-  function updateElement(type, target) {
-    let updatedElement;
-    switch (type) {
-      case 'button':
-        const buttonText = document.getElementById('buttonText').value;
-        updatedElement = `<button>${buttonText}</button>`;
-        break;
-      case 'input':
-        const inputPlaceholder = document.getElementById('inputPlaceholder').value;
-        updatedElement = `<input type="text" placeholder="${inputPlaceholder}">`;
-        break;
-      case 'textarea':
-        const textareaRows = document.getElementById('textareaRows').value;
-        updatedElement = `<textarea rows="${textareaRows}"></textarea>`;
-        break;
-      // Add more cases for other elements
-    }
-    return updatedElement;
+// Create the tools based on the selected element
+function createTools(type) {
+  let tools;
+  switch (type) {
+    case 'button':
+      tools = 'Button Tools: <input type="text" id="buttonText" placeholder="Button Text">';
+      break;
+    case 'input':
+      tools = 'Input Tools: <input type="text" id="inputPlaceholder" placeholder="Input Placeholder">';
+      break;
+    case 'textarea':
+      tools = 'Textarea Tools: <input type="number" id="textareaRows" placeholder="Number of Rows">';
+      break;
+    case 'checkbox':
+      tools = 'Checkbox Tools: <input type="text" id="checkboxLabel" placeholder="Checkbox Label">';
+      break;
+    case 'label':
+      tools = 'Label Tools: <input type="text" id="labelText" placeholder="Label Text">';
+      break;
+    // Add more cases for other elements
   }
-});
+  return tools;
+}
 
+// Update the element based on the tool changes
+function updateElement(type, target) {
+  let updatedElement;
+  switch (type) {
+    case 'button':
+      const buttonText = document.getElementById('buttonText').value;
+      updatedElement = `<button>${buttonText}</button>`;
+      break;
+    case 'input':
+      const inputPlaceholder = document.getElementById('inputPlaceholder').value;
+      updatedElement = `<input type="text" placeholder="${inputPlaceholder}">`;
+      break;
+    case 'textarea':
+      const textareaRows = document.getElementById('textareaRows').value;
+      updatedElement = `<textarea rows="${textareaRows}"></textarea>`;
+      break;
+    case 'checkbox':
+      const checkboxLabel = document.getElementById('checkboxLabel').value;
+      updatedElement = `<input type="checkbox"> ${checkboxLabel}`;
+      break;
+    case 'label':
+      const labelText = document.getElementById('labelText').value;
+      updatedElement = `<label>${labelText}</label>`;
+      break;
+    // Add more cases for other elements
+  }
+  return updatedElement;
+}
+
+// ...
