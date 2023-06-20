3ddcd8c476fa960c6b6c39678679f02e915a8d70
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-06-20 15:23:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -53,24 +53,100 @@ function createNewElement(type) {
   return element;
 }
 
+// ...
+
 // Create the tools based on the selected element
 function createTools(type) {
   let tools;
   switch (type) {
     case 'button':
-      tools = 'Button Tools: <input type="text" id="buttonText" placeholder="Button Text">';
+      tools = `
+        Button Tools:
+        <input type="text" id="buttonText" placeholder="Button Text"><br>
+        <label for="buttonBackgroundColor">Background Color:</label>
+        <input type="color" id="buttonBackgroundColor"><br>
+        <label for="buttonTextColor">Text Color:</label>
+        <input type="color" id="buttonTextColor"><br>
+        <label for="buttonFontSize">Font Size:</label>
+        <input type="number" id="buttonFontSize"><br>
+        <label for="buttonBorderRadius">Border Radius:</label>
+        <input type="number" id="buttonBorderRadius"><br>
+        <label for="buttonBoxShadow">Box Shadow:</label>
+        <input type="text" id="buttonBoxShadow" placeholder="e.g., 2px 2px 4px rgba(0, 0, 0, 0.2)">
+      `;
       break;
     case 'input':
-      tools = 'Input Tools: <input type="text" id="inputPlaceholder" placeholder="Input Placeholder">';
+      tools = `
+        Input Tools:
+        <input type="text" id="inputPlaceholder" placeholder="Input Placeholder"><br>
+        <label for="inputBackgroundColor">Background Color:</label>
+        <input type="color" id="inputBackgroundColor"><br>
+        <label for="inputTextColor">Text Color:</label>
+        <input type="color" id="inputTextColor"><br>
+        <label for="inputFontSize">Font Size:</label>
+        <input type="number" id="inputFontSize"><br>
+        <label for="inputBorderRadius">Border Radius:</label>
+        <input type="number" id="inputBorderRadius"><br>
+        <label for="inputBoxShadow">Box Shadow:</label>
+        <input type="text" id="inputBoxShadow" placeholder="e.g., 2px 2px 4px rgba(0, 0, 0, 0.2)">
+      `;
       break;
     case 'textarea':
-      tools = 'Textarea Tools: <input type="number" id="textareaRows" placeholder="Number of Rows">';
+      tools = `
+        Textarea Tools:
+        <label for="textareaRows">Number of Rows:</label>
+        <input type="number" id="textareaRows"><br>
+        <label for="textareaBackgroundColor">Background Color:</label>
+        <input type="color" id="textareaBackgroundColor"><br>
+        <label for="textareaTextColor">Text Color:</label>
+        <input type="color" id="textareaTextColor"><br>
+        <label for="textareaFontSize">Font Size:</label>
+        <input type="number" id="textareaFontSize"><br>
+        <label for="textareaBorderRadius">Border Radius:</label>
+        <input type="number" id="textareaBorderRadius"><br>
+        <label for="textareaBoxShadow">Box Shadow:</label>
+        <input type="text" id="textareaBoxShadow" placeholder="e.g., 2px 2px 4px rgba(0, 0, 0, 0.2)">
+      `;
       break;
     case 'checkbox':
-      tools = 'Checkbox Tools: <input type="text" id="checkboxLabel" placeholder="Checkbox Label">';
+      tools = `
+        Checkbox Tools:
+        <input type="text" id="checkboxLabel" placeholder="Checkbox Label"><br>
+        <label for="checkboxTextColor">Text Color:</label>
+        <input type="color" id="checkboxTextColor"><br>
+        <label for="checkboxFontSize">Font Size:</label>
+        <input type="number" id="checkboxFontSize"><br>
+        <label for="checkboxCursor">Cursor:</label>
+        <select id="checkboxCursor">
+          <option value="auto">Auto</option>
+          <option value="pointer">Pointer</option>
+          <option value="crosshair">Crosshair</option>
+          <option value="move">Move</option>
+          <!-- Add more cursor styles as needed -->
+        </select>
+      `;
       break;
     case 'label':
-      tools = 'Label Tools: <input type="text" id="labelText" placeholder="Label Text">';
+      tools = `
+        Label Tools:
+        <input type="text" id="labelText" placeholder="Label Text"><br>
+        <label for="labelTextColor">Text Color:</label>
+        <input type="color" id="labelTextColor"><br>
+        <label for="labelFontSize">Font Size:</label>
+        <input type="number" id="labelFontSize"><br>
+        <label for="labelFontWeight">Bold:</label>
+        <input type="checkbox" id="labelFontWeight"><br>
+        <label for="labelLink">Link:</label>
+        <input type="text" id="labelLink" placeholder="Label URL"><br>
+        <label for="labelCursor">Cursor:</label>
+        <select id="labelCursor">
+          <option value="auto">Auto</option>
+          <option value="pointer">Pointer</option>
+          <option value="crosshair">Crosshair</option>
+          <option value="move">Move</option>
+          <!-- Add more cursor styles as needed -->
+        </select>
+      `;
       break;
     // Add more cases for other elements
   }
@@ -83,23 +159,46 @@ function updateElement(type, target) {
   switch (type) {
     case 'button':
       const buttonText = document.getElementById('buttonText').value;
-      updatedElement = `<button>${buttonText}</button>`;
+      const buttonBackgroundColor = document.getElementById('buttonBackgroundColor').value;
+      const buttonTextColor = document.getElementById('buttonTextColor').value;
+      const buttonFontSize = document.getElementById('buttonFontSize').value;
+      const buttonBorderRadius = document.getElementById('buttonBorderRadius').value;
+      const buttonBoxShadow = document.getElementById('buttonBoxShadow').value;
+      updatedElement = `<button style="background-color: ${buttonBackgroundColor}; color: ${buttonTextColor}; font-size: ${buttonFontSize}px; border-radius: ${buttonBorderRadius}px; box-shadow: ${buttonBoxShadow}">${buttonText}</button>`;
       break;
     case 'input':
       const inputPlaceholder = document.getElementById('inputPlaceholder').value;
-      updatedElement = `<input type="text" placeholder="${inputPlaceholder}">`;
+      const inputBackgroundColor = document.getElementById('inputBackgroundColor').value;
+      const inputTextColor = document.getElementById('inputTextColor').value;
+      const inputFontSize = document.getElementById('inputFontSize').value;
+      const inputBorderRadius = document.getElementById('inputBorderRadius').value;
+      const inputBoxShadow = document.getElementById('inputBoxShadow').value;
+      updatedElement = `<input type="text" placeholder="${inputPlaceholder}" style="background-color: ${inputBackgroundColor}; color: ${inputTextColor}; font-size: ${inputFontSize}px; border-radius: ${inputBorderRadius}px; box-shadow: ${inputBoxShadow}">`;
       break;
     case 'textarea':
       const textareaRows = document.getElementById('textareaRows').value;
-      updatedElement = `<textarea rows="${textareaRows}"></textarea>`;
+      const textareaBackgroundColor = document.getElementById('textareaBackgroundColor').value;
+      const textareaTextColor = document.getElementById('textareaTextColor').value;
+      const textareaFontSize = document.getElementById('textareaFontSize').value;
+      const textareaBorderRadius = document.getElementById('textareaBorderRadius').value;
+      const textareaBoxShadow = document.getElementById('textareaBoxShadow').value;
+      updatedElement = `<textarea rows="${textareaRows}" style="background-color: ${textareaBackgroundColor}; color: ${textareaTextColor}; font-size: ${textareaFontSize}px; border-radius: ${textareaBorderRadius}px; box-shadow: ${textareaBoxShadow}"></textarea>`;
       break;
     case 'checkbox':
       const checkboxLabel = document.getElementById('checkboxLabel').value;
-      updatedElement = `<input type="checkbox"> ${checkboxLabel}`;
+      const checkboxTextColor = document.getElementById('checkboxTextColor').value;
+      const checkboxFontSize = document.getElementById('checkboxFontSize').value;
+      const checkboxCursor = document.getElementById('checkboxCursor').value;
+      updatedElement = `<label style="color: ${checkboxTextColor}; font-size: ${checkboxFontSize}px; cursor: ${checkboxCursor}"><input type="checkbox"> ${checkboxLabel}</label>`;
       break;
     case 'label':
       const labelText = document.getElementById('labelText').value;
-      updatedElement = `<label>${labelText}</label>`;
+      const labelTextColor = document.getElementById('labelTextColor').value;
+      const labelFontSize = document.getElementById('labelFontSize').value;
+      const labelFontWeight = document.getElementById('labelFontWeight').checked ? 'bold' : 'normal';
+      const labelLink = document.getElementById('labelLink').value;
+      const labelCursor = document.getElementById('labelCursor').value;
+      updatedElement = `<label style="color: ${labelTextColor}; font-size: ${labelFontSize}px; font-weight: ${labelFontWeight}; cursor: ${labelCursor}"><a href="${labelLink}">${labelText}</a></label>`;
       break;
     // Add more cases for other elements
   }
